e443f1419bd0dc3e22191a5efc7bcbae37b34b97
kirill <kirill108bol@gmail.com>
2026-08-26 18:03:33 +0200

    vercel analytics added

--- tochky/app/layout.tsx
+++ tochky/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import { NextIntlClientProvider } from "next-intl"
 import { getMessages, getLocale } from "next-intl/server"
 import { routing } from "@/i18n/routing"
+import { Analytics } from "@vercel/analytics/react"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -37,6 +38,7 @@ export default async function RootLayout({
     >
       <body suppressHydrationWarning>
         <ThemeProvider>{children}</ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
