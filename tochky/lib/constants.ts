b46acec0c76e0e279f3d705ed2f84111e9991802
kirill <kirill108bol@gmail.com>
2026-08-26 21:22:18 +0200

    feat: changed order of sizes

--- tochky/lib/constants.ts
+++ tochky/lib/constants.ts
@@ -1,27 +1,35 @@
 export const GAME_STATUS = {
-  PLAYING: 'playing',
-  FINISHED: 'finished',
-  WAITING: 'waiting',
-} as const;
+  PLAYING: "playing",
+  FINISHED: "finished",
+  WAITING: "waiting",
+} as const
 
 export const WIN_REASON = {
-  SURRENDER: 'surrender',
-  TIMEOUT: 'timeout',
-  DISCONNECT: 'disconnect',
-  BOARD_FULL: 'boardFull',
-} as const;
+  SURRENDER: "surrender",
+  TIMEOUT: "timeout",
+  DISCONNECT: "disconnect",
+  BOARD_FULL: "boardFull",
+} as const
 
 export const CONTROL_SCHEME = {
-  DIRECT: 'direct',
-  DRAG: 'drag',
-  CONFIRM: 'confirm',
-} as const;
+  DIRECT: "direct",
+  DRAG: "drag",
+  CONFIRM: "confirm",
+} as const
 
-export const BOARD_SIZES = ['20x20', '30x30', '20x30', '39x32', '39x39'] as const;
-export const DEFAULT_BOARD_WIDTH = 20;
-export const DEFAULT_BOARD_HEIGHT = 20;
-export const DEFAULT_BOARD_SIZE = `${DEFAULT_BOARD_WIDTH}x${DEFAULT_BOARD_HEIGHT}` as const;
+export const BOARD_SIZES = [
+  "20x20",
+  "20x30",
+  "30x30",
+  "39x32",
+  "39x39",
+] as const
+export const DEFAULT_BOARD_WIDTH = 20
+export const DEFAULT_BOARD_HEIGHT = 20
+export const DEFAULT_BOARD_SIZE =
+  `${DEFAULT_BOARD_WIDTH}x${DEFAULT_BOARD_HEIGHT}` as const
 
-export type GameStatusType = typeof GAME_STATUS[keyof typeof GAME_STATUS];
-export type WinReasonType = typeof WIN_REASON[keyof typeof WIN_REASON];
-export type ControlSchemeType = typeof CONTROL_SCHEME[keyof typeof CONTROL_SCHEME];
+export type GameStatusType = (typeof GAME_STATUS)[keyof typeof GAME_STATUS]
+export type WinReasonType = (typeof WIN_REASON)[keyof typeof WIN_REASON]
+export type ControlSchemeType =
+  (typeof CONTROL_SCHEME)[keyof typeof CONTROL_SCHEME]
